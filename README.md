bb97f7583a9fc65a2634942eb09449f3bfe74b99
Marzena Włoczyk <mk.wloczyk@gmail.com>
2017-08-13 21:25:48 +0200

    new README, modification: package.json and formul.jsx

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# swiadkowie44-website
+# swiadkowie44 website #
+
 Website dedicated to the living Heroes of the Warsaw Uprising - interviews with people who saw and made history. 
+
+## Inspiration ##
+
+63rd anniversary of Warsaw Uprising - with every anniversary less and less veterans can share the story with us. It's time to put there stories in one place for next generations.
+
+## Features ##
+
+* vivid interviews with veterans who took part in the most dramatic skirmishes of the Warsaw Uprising
+* original map of Warsaw from 1944
+* veteran alert form - users can use this form to alert our crew about their family member, friend or neightbour who took part in the Warsaw Uprising
+* sliders with original photos from 1944
+
+## Built With ##
+
+* Webpack
+* ReactJS
+* ES6 with babel
+* Stylesheets - SASS and CSS
+* Automatic code splitting using multiple entry points
+* PostCSS - Auto-Prefixer
+
+
+## Install ##
+
+You can get it on npm:
+
+1. npm install
+2. npm start
+3. and look at this website on localhost
+
--- js/components/formul.jsx
+++ js/components/formul.jsx
@@ -28,6 +28,9 @@ export class Formul extends React.Component {
             tex: event.target.value
         })
     }
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
     render() {
       return (
           <div className="text">
@@ -35,7 +38,7 @@ export class Formul extends React.Component {
               <p>
               Jeżeli Twoja babcia, ciocia, sąsiadka czy znajoma, albo Twój dziadek, tata, kuzyn lub podopieczny brali udział w Powstaniu Warszawskim, a ich historia jest niezwykła, albo zwyczajnie piękna, przedstaw nas sobie! Chętnie Ich poznamy, wysłuchamy i zapiszemy Ich wspomnienia. Nie szkodzi, że czują się przeciętni. Uczestniczyli w nieprzeciętnym wydarzeniu i warto, by ich opowieść pozostała zachowana. Ku pamieci, dla rodzin, bliskich, dla przyszłych pokoleń.
               </p>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                     <div className="contact">
                         <div className="element">
                             <label className="top"> Podaj swoje imię </label>
